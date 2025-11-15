04ad51dd51a6d6b23c0e94c68c4f7cc8f6c05bb6
VictorSdePasta <victor.delima@sptech.school>
2025-11-15 13:01:25 -0300

    ajustes no funcionamento de cadastro e login

--- public/js/validacoes.js
+++ public/js/validacoes.js
@@ -9,49 +9,47 @@ function cadastrar() {
     if (emailUser == "") { divErroEmail.innerHTML = `O campo email está vazio.` }
     if (senhaUser == "") { divErroSenha.innerHTML = `O campo senha está vazio.` }
     if (confirmarSenhaUser == "") { divErroConfirmar.innerHTML = `O campo confirmar senha está vazio.` }
-
+  } else {
     if (senhaUser != confirmarSenhaUser) {
-      divErroNome.innerHTML = `As senhas não coincidem.`
-
+      divErroSenha.innerHTML = `As senhas não coincidem.`
+    } else {
       if (validar(emailUser, senhaUser)) {
-        return
-      }
-      
-      fetch("/usuarios/cadastrar", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          nomeServer: nomeUser,
-          emailServer: emailUser,
-          senhaServer: senhaUser
+        fetch("/usuarios/cadastrar", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            nomeServer: nomeUser,
+            emailServer: emailUser,
+            senhaServer: senhaUser
+          })
+        }).then(function (resposta) {
+          if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+              console.log(json);
+              console.log(JSON.stringify(json));
+
+              divValidacao.innerHTML = `Cadastro realizado, entre para o terror!`
+
+              setTimeout(function () {
+                window.location = "./login.html";
+              }, 3000);
+            });
+          } else {
+            console.log("Houve um erro ao tentar realizar o cadastro!");
+
+            resposta.text().then(texto => {
+              console.error(texto);
+              finalizarAguardar(texto);
+            });
+          }
+        }).catch(function (erro) {
+          console.log(erro);
         })
-      }).then(function (resposta) {
-        if (resposta.ok) {
-          console.log(resposta);
-
-          resposta.json().then(json => {
-            console.log(json);
-            console.log(JSON.stringify(json));
-
-            divValidacao.innerHTML = `Cadastro realizado, entre para o terror!`
-
-            setTimeout(function () {
-              window.location = "./login.html";
-            }, 1000);
-          });
-        } else {
-          console.log("Houve um erro ao tentar realizar o cadastro!");
-
-          resposta.text().then(texto => {
-            console.error(texto);
-            finalizarAguardar(texto);
-          });
-        }
-      }).catch(function (erro) {
-        console.log(erro);
-      })
+      }
     }
   }
 }
@@ -84,18 +82,19 @@ function entrar() {
           sessionStorage.NOME_USUARIO = json.nome;
           sessionStorage.ID_USUARIO = json.id;
 
-          window.location = "./dashboard/cards.html";
+          window.location = "./dashboard/feed.html";
         });
 
       } else {
         console.log("Houve um erro ao tentar realizar o login!");
         resposta.text().then(texto => {
           console.error(texto);
-          divErro.innerHTML = `Algum erro inesperado ocorreu, tente novamente mais tarde.`
+          divErro.innerHTML = `Email e/ou senha estão incorretos.`
         });
       }
-
+      
     }).catch(function (erro) {
+      divErro.innerHTML = `Algum erro inesperado ocorreu, tente novamente mais tarde.`
       console.log(erro);
     });
   }
@@ -103,10 +102,10 @@ function entrar() {
 
 function validar(emailUser, senhaUser) {
   let emailValido = false
-  let senhaValido = true
+  let senhaValido = false
   let atPosition = emailUser.indexOf('@')
 
-  if (atPosition <= 0) {
+  if (atPosition >= 0) {
     for (let i = 0; i < emailUser.length; i++) {
       if (emailUser[i] == '.' && atPosition < i) {
         emailValido = true
@@ -117,6 +116,8 @@ function validar(emailUser, senhaUser) {
 
   if (!emailValido) {
     divErroEmail.innerHTML = `Digite um email válido.`
+  } else {
+    divErroEmail.innerHTML = ``
   }
 
   if (senhaUser.toUpperCase() == senhaUser.toLowerCase() || senhaUser.toUpperCase() == senhaUser.toUpperCase()) {
@@ -124,19 +125,21 @@ function validar(emailUser, senhaUser) {
   }
 
   for (var i = 0; i < senhaUser.length; i++) {
-    if ('0123456789'.indexOf(senhaUser[i]) === -1) {
-      senhaValido = false
+    if ('0123456789'.indexOf(senhaUser[i]) == -1) {
+      senhaValido = true
     }
-    if (`\|!¹@²#³$£%¢¨¬&*()-_=+§´[{}]ªº^~<,>.:;?/°`.indexOf(senhaUser[i]) === -1) {
-      senhaValido = false
+    if (`|!¹@²#³$£%¢¨¬&*()-_=+§´[{}]ªº^~<,>.:;?/°`.indexOf(senhaUser[i]) == -1) {
+      senhaValido = true
     }
-    if ('`'.indexOf(senhaUser[i]) === -1) {
-      senhaValido = false
+    if ('`'.indexOf(senhaUser[i]) == -1) {
+      senhaValido = true
     }
   }
 
   if (!senhaValido) {
     divErroSenha.innerHTML = `A senha deve conter uma letra maiúscula, uma minúscula, um número e um carácter especial.`
+  } else {
+    divErroSenha.innerHTML = ``
   }
 
   if (emailValido && senhaValido) {
